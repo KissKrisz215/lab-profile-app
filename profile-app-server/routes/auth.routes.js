4c726730e778ea45d84861b437bc697000e8f28e
Krisztian Kiss <kisskrisztian215@gmail.com>
2023-06-11 22:33:31 +0200

    Finished Lab

--- profile-app-server/routes/auth.routes.js
+++ profile-app-server/routes/auth.routes.js
@@ -3,19 +3,19 @@ const User = require('../models/User.model');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 const saltRounds = 10;
-const {isAuthenticated} = require("../middlewares/jwt.middleware");
+const {isAuthenticated} = require("../middlewares/jwt.middleware")
 
 router.post("/signup", (req,res,next) => {
-    const {password,username} = req.body;
-
-    if(password === undefined || username === undefined){
-        res.status(400).json({message: 'Please Provide Inputs'});
+    const {password,username,campus,course} = req.body;
+    if(password === undefined || username === undefined || campus === undefined || course === undefined){
+        res.status(400).json({message: 'Please Provide Inputs1'});
         return;
     }
 
-    if(password === '' || username === ''){
-        res.status(400).json("Please provide Inputs");
+    if(password === '' || username === '' || campus === '' || course === ''){
+        res.status(400).json({message: "Please provide Inputs2"});
         return;
     }
 
@@ -34,7 +34,8 @@ router.post("/signup", (req,res,next) => {
 
         const salt = bcrypt.genSaltSync(saltRounds);
         const hashedPassword = bcrypt.hashSync(password, salt);
-        return User.create({password: hashedPassword,username})
+        const imageUrl = 'https://res.cloudinary.com/dyayaq1ee/image/upload/v1686513554/movie-gallery/z4cbw0irhkdqrtioxebc.png'
+        return User.create({password: hashedPassword,username, campus, course, image: imageUrl})
 
     })
     .then((createdUser) => {
@@ -69,7 +70,7 @@ router.post("/login", (req,res,next) => {
         const passwordCompare = bcrypt.compareSync(password, foundUser.password);
 
         if(passwordCompare){
-            const {_id, username} = foundUser;
+            const {_id, username, campus, course} = foundUser;
 
             const payload = {_id, username};
 
@@ -93,5 +94,6 @@ router.get("/verify",isAuthenticated, (req,res,next) => {
    console.log(req.payload);
    res.status(200).json(req.payload);
 })
+
 
 module.exports = router;
--- profile-app-server/routes/index.routes.js
+++ profile-app-server/routes/index.routes.js
@@ -1,7 +1,66 @@
 const router = require("express").Router();
+const User = require('../models/User.model');
+const fileUploader = require("../config/cloudinary.config");
+const {isAuthenticated} = require("../middlewares/jwt.middleware");
+const mongoose = require("mongoose");
 
 router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
+
+router.post("/upload", fileUploader.single("imageUrl"), (req,res,next) => {
+
+  const {_id} = req.payload;
+
+
+  if(!req.file){
+      next(new Error("No file uploaded!"));
+      return;
+  }
+
+  if(!mongoose.Types.ObjectId.isValid(_id)){
+    res.status(400).json("Id is Invalid");
+    return;
+}
+
+  User.findByIdAndUpdate(_id, {image: req.file.path}, {new: true})
+  .then(() => console.log("User Successfully Updated"))
+  .catch((err) => console.log("Error Updating", err))
+
+  res.json({fileUrl: req.file.path})
+})
+
+router.get("/user", isAuthenticated, (req,res,next) => {
+  const {_id} = req.payload;
+  User.findById(_id)
+  .then((foundUser) => {
+      const {username, course, campus, image} = foundUser;
+      const user = {
+          username,
+          course,
+          campus,
+          image
+      }
+      res.status(200).json({user: user});
+  })
+  .catch((err) => {
+      res.status(500).json({message: "Internal Server Error"});
+  })
+})
+
+router.put("/user",isAuthenticated, (req,res,next) => {
+  const {_id} = req.payload;
+  const {username, campus, course} = req.body;
+  
+  if(!mongoose.Types.ObjectId.isValid(_id)){
+      res.status(400).json("Id is Invalid");
+      return;
+  }
+
+  User.findByIdAndUpdate(_id, req.body, {new:true})
+  .then((response) => res.json(response))
+  .catch((err) => res.json(err));
+
+})
 
 module.exports = router;
